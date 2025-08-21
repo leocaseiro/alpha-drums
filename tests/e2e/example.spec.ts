ede0233999e22717d7306bd6489352989599ede2
leocaseiro <leobok@gmail.com>
2025-08-22 00:43:13 +1000

    update nvm to 20.19, and fix playrwright tests

--- tests/e2e/example.spec.ts
+++ tests/e2e/example.spec.ts
@@ -2,5 +2,5 @@ import { test, expect } from '@playwright/test';
 
 test('should have a heading', async ({ page }) => {
   await page.goto('/');
-  await expect(page.getByRole('heading', { name: 'Welcome to Next.js!' })).toBeVisible();
+  await expect(page.getByRole('heading', { name: 'Alpha Drums' })).toBeVisible();
 });
